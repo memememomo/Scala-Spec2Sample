70f61652c38b95f88e230deda5af60d435bb47cb
Uchiko <memememomo@gmail.com>
2016-02-28 22:11:22 +0900

    Add test case

--- src/test/scala/RabbitSpec.scala
+++ src/test/scala/RabbitSpec.scala
@@ -4,14 +4,23 @@ class RabbitSpec extends Specification {
 
   "うさぎ" should {
 
-    "ジャンプできる" in {
-      val rabbit = new Rabbit("月野うさぎ")
-      rabbit.jump must_== "月野うさぎ jumped!"
+    "日本名のうさぎ" should {
+      "ジャンプできる" in {
+        val rabbit = new Rabbit("月野うさぎ")
+        rabbit.jump must_== "月野うさぎ jumped!"
+      }
+
+      "話すことができない" in {
+        val rabbit = new Rabbit("月野うさぎ")
+        rabbit.talk must throwAn[UnsupportedOperationException]
+      }
     }
 
-    "話すことができない" in {
-      val rabbit = new Rabbit("月野うさぎ")
-      rabbit.talk must throwAn[UnsupportedOperationException]
-    }
-  }
+    "英名のうさぎ" should {
+      "ジャンプできる" in {
+        val rabbit = new Rabbit("Peter Rabbit")
+        rabbit.jump must_== "Peter Rabbit jumped!"
+      }
+   }
+ }
 }
